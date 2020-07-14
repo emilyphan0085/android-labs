02a912b2dbfc6bfe4dafec9057237871d80804e1
Emily <phan0085@algonquinlive.com>
2020-07-14 00:49:49 -0400

    lab3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main_layout);
+
+        EditText email = findViewById(R.id.editText1);
+        Button saveButton = (Button) findViewById(R.id.button);
+        Intent nextPage = new Intent(MainActivity.this, ProfileActivity.class);
+        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        String savedString = prefs.getString("ReserveName", "");
+        email.setText(savedString);
+        saveButton.setOnClickListener(click-> {
+            saveSharedPrefs(email.getText().toString());
+            nextPage.putExtra("EMAIL", email.getText().toString());
+            startActivity(nextPage);
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    private void saveSharedPrefs(String stringToSave)
+    {
+       SharedPreferences prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("TypedText", stringToSave);
+        editor.commit();
+
+
     }
 }
+
